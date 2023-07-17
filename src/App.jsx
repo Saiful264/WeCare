eba0f8ce8d659c05d0022f4cef20829c0fa96fdb
Saiful264 <saifulislamx264@gmail.com>
2023-07-17 23:19:14 +0600

    banner part 80% completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import "./App.css";
 import Navbar from "./componet/Navbar";
 import bannerImg from "./assets/3421663_6966 1.png";
+import C_logo from "./assets/calendar.png"
+import L_logo from "./assets/location.png"
+import S_logo from "./assets/shield.png"
+import U_logo from "./assets/user.png"
+
 
 function App() {
   return (
@@ -11,34 +16,34 @@ function App() {
           <div className="container">
             <div className="banner">
               <div>
-                <h1>Protect your health and take care of your health</h1>
+                <h1 className="banner-title">Protect your health and take care of your health</h1>
                 <div className="service">
-                  <div>
-                    <span></span>
-                    <div>
-                      <h3>Make an appointment</h3>
-                      <p>Schedule with your favorite doctor anythime</p>
+                  <div className="service-item">
+                    <img src={C_logo} alt="" />
+                    <div className="service-content">
+                      <h3 className="service-tatile">Make an appointment</h3>
+                      <p className="service-datile">Schedule with your favorite doctor anythime</p>
                     </div>
                   </div>
-                  <div>
-                    <span></span>
-                    <div>
-                      <h6>Health guarantee forever</h6>
-                      <p>We always provide the dest warranty for you</p>
+                  <div className="service-item">
+                    <img src={S_logo} alt="" />
+                    <div className="service-content">
+                      <h6 className="service-tatile">Health guarantee forever</h6>
+                      <p className="service-datile">We always provide the dest warranty for you</p>
                     </div>
                   </div>
-                  <div>
-                    <span></span>
-                    <div>
-                      <h6>Find your dest doctor</h6>
-                      <p>We always provide the dest warranty for you</p>
+                  <div className="service-item">
+                    <img src={U_logo} alt="" />
+                    <div className="service-content">
+                      <h6 className="service-tatile">Find your dest doctor</h6>
+                      <p className="service-datile">We always provide the dest warranty for you</p>
                     </div>
                   </div>
-                  <div>
-                    <span></span>
-                    <div>
-                      <h6>Spread in various place</h6>
-                      <p>Now clinics are available in various places</p>
+                  <div className="service-item">
+                    <img src={L_logo} alt="" />
+                    <div className="service-content">
+                      <h6 className="service-tatile">Spread in various place</h6>
+                      <p className="service-datile">Now clinics are available in various places</p>
                     </div>
                   </div>
                 </div>
